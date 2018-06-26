b36bb6e761f283bf83ff7b222dd97879ddf42f17
Sebastian Schürmann <sebs@2xs.org>
2018-06-26 16:57:52 +0200

    Implement subsets

--- packages/aowatch-client/client.js
+++ packages/aowatch-client/client.js
@@ -1,25 +1,27 @@
 import "isomorphic-fetch"
 export class Client {
+
+	_fetch(url) {
+		return fetch(url)
+			.then(function(response) {
+				return response.json();
+			})
+	}
 	profile(firstname, lastname) {
 		const lookupName = firstname.toLowerCase() + '-' + lastname.toLowerCase();
 		const url = `https://www.abgeordnetenwatch.de/api/parliament/bundestag/profile/${lookupName}/profile.json`;
-		return fetch(url)
-		.then(function(response) {
-			return response.json();
-		})
+		return this._fetch(url);
 	}
 	allParliaments() {
 		const url = 'https://www.abgeordnetenwatch.de/api/parliaments.json';
-		return fetch(url)
-			.then(function(response) {
-				return response.json();
-			})
+		return this._fetch(url);
 	}
 	parliament(state, type) {
 		const url = `https://www.abgeordnetenwatch.de/api/parliament/${state}/${type}.json`
-		return fetch(url)
-			.then(function(response) {
-				return response.json();
-			})
+		return this._fetch(url);
+	}
+	subset(state, type, subset) {
+		const url = `https://www.abgeordnetenwatch.de/api/parliament/${state}/${type}.json?subsets=${subset}`
+		return this._fetch(url);
 	}
 };
--- packages/aowatch-client/test/simple.js
+++ packages/aowatch-client/test/simple.js
@@ -5,19 +5,26 @@ var assert = require('assert');
 describe('client', function() {
   it('does a simple request', (done)=>{
     const client  = new Client();
-    client.parliament('hamburg', 'deputies').then(()=>{
+    client.parliament('hamburg', 'deputies').then((res)=>{
       done();
     });
   })
+  it('does a subset', (done)=>{
+    const client  = new Client();
+    client.subset('hamburg', 'deputies', 'votes').then((res)=>{
+      done();
+    });
+  })
+  
   it('does a simple all request', (done)=>{
     const client  = new Client();
-    client.allParliaments('hamburg', 'deputies').then(()=>{
+    client.allParliaments('hamburg', 'deputies').then((res)=>{
       done();
     });
   })
   it('does a profile request', (done)=>{
     const client  = new Client();
-    client.profile('Angela', 'Merkel').then(()=>{
+    client.profile('Angela', 'Merkel').then((res)=>{
       done();
     });
   })
