8b3412813ad0ac82aacfb6fed6ca225637caae3b
Sebastian Schürmann <sebs@2xs.org>
2018-06-26 17:13:49 +0200

    Added a base app

---
+++ packages/union-watch/src/union-watch-app/union-watch-app.js
@@ -0,0 +1,28 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * @customElement
+ * @polymer
+ */
+class UnionWatchApp extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'union-watch-app'
+      }
+    };
+  }
+}
+
+window.customElements.define('union-watch-app', UnionWatchApp);
