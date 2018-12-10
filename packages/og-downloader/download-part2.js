fd2d8d11025522dc052ecac860fb3ef3ceb1a568
Sebastian Schürmann <sebs@2xs.org>
2018-12-10 21:06:52 +0100

    Simple downloader for both datasets from offenegesetze.de

---
+++ packages/og-downloader/download-part2.js
@@ -0,0 +1,8 @@
+const { DownloaderHelper } = require('node-downloader-helper');
+const downloadDirName = __dirname + '/downloads'
+const dl = new DownloaderHelper('https://media.offenegesetze.de/bgbl2.tar.bz2', downloadDirName);
+dl.on('progress', (e)=> {
+    console.log('rest', e.total - e.downloaded)
+})
+dl.on('end', () => console.log('Download Completed'))
+dl.start();
